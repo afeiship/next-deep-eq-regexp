af6382d261873779a25028dc0dbe16f440ad4707
afeiship <afeiship@gmail.com>
2019-01-12 01:42:34 +0800

    feat: inti & done

---
+++ README.md
@@ -0,0 +1,12 @@
+# next-deep-eq-regexp
+> Equal regexp for next
+
+## install:
+```bash
+npm install -S afeiship/next-deep-eq-regexp --registry=https://registry.npm.taobao.org
+```
+
+## usage:
+```js
+//DOCS here!
+```
---
+++ dist/next-deep-eq-regexp.js
@@ -0,0 +1,26 @@
+(function() {
+  var global = global || this || window || Function('return this')();
+  var nx = global.nx || require('next-js-core2');
+  var isRegExp = function(target) {
+    return target instanceof RegExp;
+  };
+
+  nx.deepEqRegexp = function(inReg1, inReg2) {
+    if (inReg1 === inReg2) {
+      return true;
+    }
+
+    var isRegExp1 = isRegExp(inReg1);
+    var isRegExp2 = isRegExp(inReg2);
+    if (isRegExp1 != isRegExp2) {
+      return false;
+    }
+    if (isRegExp1 && isRegExp2) {
+      return inReg1.getTime() === inReg1.getTime();
+    }
+  };
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = nx.deepEqRegexp;
+  }
+})();
